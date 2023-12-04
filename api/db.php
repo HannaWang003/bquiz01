9474ddb5a048438c74e5061f88925dcf6bcd730d
Hanna <hannawang003@gmail.com>
2023-12-04 16:31:43 +0800

    繼續完成db.php

--- api/db.php
+++ api/db.php
@@ -13,4 +13,19 @@ function to($url)
 }
 class DB
 {
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db03";
+    protected $pdo;
+    protected $table;
+    function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+    function all{
+
+    }
+    private a2s($where){
+        foreach($where)
+    }
 }
+$Data = new DB($table);

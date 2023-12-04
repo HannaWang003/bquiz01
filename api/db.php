8cf0c93079459b0da096db3e64a8014e0d3753b1
Hanna <hannawang003@gmail.com>
2023-12-04 16:19:41 +0800

    切版/中間畫面切換(front/back)/連結路徑修改/db.php檔建立

---
+++ api/db.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set('Asia/Taipei');
+session_start();
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+function to($url)
+{
+    header("location:$url");
+}
+class DB
+{
+}

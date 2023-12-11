b4c09ef363f383f972c445973a661c57c9029f14
HannaWang003 <hannawang003@gmail.com>
2023-12-12 01:41:30 +0800

    feat:完成頁尾版權資料管理

--- practice/api/edit_info.php
+++ practice/api/edit_info.php
@@ -3,7 +3,7 @@ include_once "db.php";
 $table = $_POST['table'];
 $DB = ${ucfirst($table)};
 $data = $DB->find(1);
-$data['total'] = $_POST[$table];
+$data[$table] = $_POST[$table];
 $DB->save($data);
 to("../back.php?do=$table");
 
--- practice/back/bottom.php
+++ practice/back/bottom.php
@@ -2,14 +2,14 @@
 $DB = ${ucfirst($do)};
 ?>
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">進站總人數管理</p>
+    <p class="t cent botli">頁尾版權資料管理管理</p>
     <form method="post" action="./api/edit_info.php">
         <table width="80%" style="margin:auto">
             <tbody>
                 <tr class="yel">
-                    <td width="50%">進站總人數</td>
+                    <td width="50%">頁尾版權資料</td>
                     <td width="50%">
-                    <input type="number" name="total" value="<?=$DB->find(1)[$do]?>">
+                        <input type="text" name="<?=$do?>" value="<?=$DB->find(1)[$do]?>">
                         <input type="hidden" name="table" value="<?=$do?>">
                     </td>
                 </tr>

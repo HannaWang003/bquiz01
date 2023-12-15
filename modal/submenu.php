d879deb420c77b47ef802414a58635e93d874ba3
Hanna <hannawang003@gmail.com>
2023-12-15 08:54:24 +0800

    撈出次選單資料

--- modal/submenu.php
+++ modal/submenu.php
@@ -1,4 +1,4 @@
-<h3>新增次選單</h3>
+<h3>編輯次選單</h3>
 <hr>
 <form action="./api/add_submenu.php" method="post" enctype="multipart/form-data">
     <table class="cent">
@@ -7,16 +7,32 @@
             <td>次選單連結網址</td>
             <td>刪除</td>
         </tr>
+        <?php
+        $subs = $Menu->all(['menu_id' => $_GET['id']]);
+        foreach ($subs as $sub) {
+        ?>
         <tr>
-            <td><input type="text" name="text[]" id=""></td>
-            <td><input type="text" name="href[]" id=""></td>
-            <td><input type="checkbox" name="del[]" id=""></td>
+            <td><input type="text" name="text[]" value="<?= $sub['text'] ?>"></td>
+            <td><input type="text" name="href[]" value="<?= $sub['href'] ?>"></td>
+            <td><input type="checkbox" name="del[]" value="<?= $sub['id'] ?>"></td>
         </tr>
+        <?php
+        }
+        ?>
     </table>
     <div>
         <input type="hidden" name="table" value="<?= $_GET['table'] ?>">
         <input type="submit" value="修改確定">
         <input type="reset" value="重置">
-        <input type="button" value="更多次選單">
+        <input type="button" value="更多次選單" onclick="more()">
     </div>
 </form>
+<script>
+function more() {
+    let item = `<tr>
+    <td><input type="text" name="text[]" value=""></td>
+    <td><input type="text" name="href[]" value=""></td>
+    </tr>`;
+    $("#xxx").append(item);
+}
+</script>

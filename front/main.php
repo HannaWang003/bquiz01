3ec8e584ca35f5f12b132fac0998adb0c87e00c4
Hanna <hannawang003@gmail.com>
2023-12-15 15:39:54 +0800

    完成在main.php顯示最新消息畫面 - 接著做more連結到的news.php畫面

--- front/main.php
+++ front/main.php
@@ -37,6 +37,13 @@
     <div
         style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php
+            if ($News->count(['sh' => 1]) > 5) {
+                echo "<a href='?do=news' style='float:right'>more...</a>";
+            }
+
+
+            ?>
         </span>
         <ul class="ssaa" style="list-style-type:decimal;">
             <?php
@@ -44,6 +51,9 @@
             foreach ($news as $n) {
                 echo "<li>";
                 echo mb_substr($n['text'], 0, 20);
+                echo  "<div class='all' style='display:none'>";
+                echo $n['text'];
+                echo "</div>";
                 echo "...";
             }
             ?>

c67b4be0805d8ab9290e35fd3ab282343457e999
HannaWang003 <hannawang003@gmail.com>
2023-12-16 22:47:59 +0800

    index, 進站人數及頁尾版權的顯示

--- practice/api/add.php
+++ practice/api/add.php
@@ -12,6 +12,11 @@ if(isset($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../img/{$_FILES['img']['name']}");
     $_POST['img']=$_FILES['img']['name'];
 }
+
+if($table != 'admin'){
+    $_POST['sh']=($table == 'title')?0:1;
+}
+
 $DB->save($_POST);
 to("../back.php?do=$table")
 

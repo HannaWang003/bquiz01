66f4ce13fda6322b2ad56abbfc772b4d6b98a3e0
HannaWang003 <hannawang003@gmail.com>
2023-12-16 18:24:50 +0800

    完成edit.php

--- practice/api/edit.php
+++ practice/api/edit.php
@@ -3,31 +3,36 @@ include_once "db.php";
 $table = $_POST['table'];
 $DB = ${ucfirst($table)};
 unset($_POST['table']);
-if(isset($_POST['del'])){
-    foreach($_POST['del'] as $id=>$val){
-        $DB->del($id);
-    }
-}
-
 foreach($_POST['id'] as $key => $id){
     if(isset($_POST['del']) && in_array($id,$_POST['del'])){
-        // echo $id;
-        $DB->del($id);        
+        $DB->del($id);
     }
+
     else{
 $row = $DB->find($id);
 // dd($row);
 // exit();
 if(isset($row['text'])){
-    $row['text']=$_POST['text'][$key];
+    $row['text'] = $_POST['text'][$key];
 }
- switch($table){
-        case "title":
-            $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
-            break;
-        default;
-    }
+switch($table){
+    case "title":
+        $row['sh'] = (isset($_POST['sh']) && $id==$_POST['sh'])?1:0;
+        break;
+    case "menu":
+        $row['href'] = $_POST['href'][$key];
+        $row['sh'] = (isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+        break;
+    case "admin":
+        $row['acc'] = $_POST['acc'][$key];
+        $row['pw'] = $_POST['pw'][$key];
+        break;
+    default:
+    $row['sh'] = (isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
 }
 $DB->save($row);
     }
+    
+}
+to("../back.php?do=$table");
 ?>

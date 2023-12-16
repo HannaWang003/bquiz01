422ca92760c4ee2b136654d97797c9db7d3c9565
HannaWang003 <hannawang003@gmail.com>
2023-12-16 18:27:01 +0800

    修正upload的submit按鈕名稱_更新

--- practice/modal/upload.php
+++ practice/modal/upload.php
@@ -34,7 +34,7 @@ switch($_GET['table']){
     <input type="hidden" name="table" value="<?=$_GET['table']?>">
     <input type="hidden" name="id" value="<?=$_GET['id']?>">
     <div>
-        <input type="submit" value="新增">
+        <input type="submit" value="更新">
         <input type="reset" value="重置">
     </div>
 </form>

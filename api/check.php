ee82396915a10d9296cff233fda3a6430dadcdcf
Hanna <hannawang003@gmail.com>
2023-12-18 13:46:32 +0800

    完成登入功能

---
+++ api/check.php
@@ -0,0 +1,9 @@
+<?php
+include_once "db.php";
+$check = $Admin->count(['acc' => $_POST['acc'], 'pw' => $_POST['pw']]);
+if ($check > 0) {
+    $_SESSION['acc'] = $_POST['acc'];
+    to("../back.php");
+} else {
+    to("index.php?do=login&err=登入錯誤");
+}

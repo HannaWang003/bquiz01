177b2bcfc649cab259b1b303c739f7fa1632bcc3
Hanna <hannawang003@gmail.com>
2023-12-18 10:33:51 +0800

    主選單區-撈出主選單

--- index.php
+++ index.php
@@ -35,6 +35,17 @@ include_once "./api/db.php";
                 <div id="menuput" class="dbor">
                     <!--主選單放此-->
                     <span class="t botli">主選單區</span>
+                    <?php
+                    $mainmu = $Menu->all(['sh' => 1, 'menu_id' => 0]);
+                    foreach ($mainmu as $main) {
+                    ?>
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=title">
+                        <div class="mainmu">
+                            <?=$main['text']?> </div>
+                    </a>
+                    <?php
+                    }
+                    ?>
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :

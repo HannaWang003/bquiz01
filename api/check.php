10e7bc19cfed2ad85382414a0e94ade17dd2f8c6
Hanna <hannawang003@gmail.com>
2024-02-26 08:47:20 +0800

    註解

--- api/check.php
+++ api/check.php
@@ -1,10 +1,11 @@
 <?php
 include_once "db.php";
+//' OR 1=1;  <=SQL注入
 $acc = htmlspecialchars($_POST['acc']);
 $pw = htmlspecialchars($_POST['pw']);
 $check = $Admin->count(['acc' => $acc, 'pw' => $pw]);
 if ($check > 0) {
-    $_SESSION['acc'] = $_POST['acc'];
+    $_SESSION['acc'] = $acc;
     to("../back.php");
 } else {
     to("../index.php?do=login&err=登入錯誤");

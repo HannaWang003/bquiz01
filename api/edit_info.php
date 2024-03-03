ae81f577305da0a620924e94f958397b72855ec7
HannaWang003 <hannawang003@gmail.com>
2024-03-03 14:50:19 +0800

    practice2 init

--- api/edit_info.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "db.php";
-$table = $_POST['table'];
-$data = ${ucfirst($table)}->find(1);
-// dd($data);
-// exit();
-$data[$table] = $_POST[$table];
-${ucfirst($table)}->save($data);
-to("../back.php?do={$table}");
--- api/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['acc']);
-header("location:../index.php");

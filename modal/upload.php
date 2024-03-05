1560c3238f082853046fd16d5b0c7d1d426d7436
HannaWang003 <hannawang003@gmail.com>
2024-03-06 00:40:34 +0800

    登入錯誤訊息改用alert

--- modal/upload.php
+++ modal/upload.php
@@ -5,3 +5,12 @@ if(isset($_GET['id'])){
     echo  "<h1>新增標題區圖片</h1>";
 }
 ?>
+<hr>
+<form action="../api/upload.php" method="post" enctype="multipart/form-data">
+<div>標題區圖片:    <input type="file" name="img"></div>
+<div>標題區替代文字:    <input type="text" name="text"></div>
+<div>
+    <input type="submit" value="<?=(isset($_GET['id']))?"更新":"新增"?>">
+    <input type="reset" value="重置">
+</div>
+</form>

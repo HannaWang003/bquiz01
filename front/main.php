91e0671b1b9b7628997c117ccdbfe36b2a90213b
HannaWang003 <hannawang003@gmail.com>
2024-03-10 05:17:56 +0800

    最新消息的more不能用三元運算式

--- front/main.php
+++ front/main.php
@@ -33,6 +33,19 @@ foreach($rows as $row){
                     		<span class="t botli">最新消息區
                             								</span>
                             <ul class="ssaa" style="list-style-type:decimal;">
+							<?php
+if(($News->count(['sh'=>1])>5)){
+	echo "<a class='bl' href='?do=news' style='float:right'>more...</a>";
+}
+	
+$news = $News->all(['sh'=>1],"limit 5");
+foreach($news as $new){
+	echo "<li>";
+	echo mb_substr($new['text'],0,20);
+	echo "</li>";
+}
+
+?>
 
                             	                            </ul>
         			<div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
